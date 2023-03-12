1b880ce56c6ae95f8063e8e9321846fcdd72a27b
Saurabh Wadekar <55120484+saurabhwadekar@users.noreply.github.com>
2023-03-12 21:07:00 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name = "flet_route",
-    version = "0.1.3",
+    version = "0.1.4",
     author="Saurabh Wadekar [ INDIA ]",
     packages=["flet_route"],
     license="MIT",
     requires=["flet","repath"],
     maintainer="Saurabh Wadekar",
     maintainer_email="saurabhwadekar420@gmail.com",
-    keywords=["flet","routing","flet simple routing"],
+    keywords=["flet","routing","flet_route","routes","flet app","flet-route","flet simple routing"],
     description="This makes it easy to manage multiple views with dynamic routing.",
     long_description=long_description,
     long_description_content_type='text/markdown',
