14e04bcd0f3223d2675b41afd196cd01fabd7d92
Saurabh Wadekar <55120484+saurabhwadekar@users.noreply.github.com>
2023-04-03 20:25:04 +0530

    middleware implemention
    1) main middleware
    2) url besed middleware

--- flet_route/routing.py
+++ flet_route/routing.py
@@ -3,9 +3,16 @@ from repath import match
 from .not_found_view import ViewNotFound
 from .params import Params
 from .basket import Basket
+from typing import Callable
 
 
-def path(url: str, clear: bool, view: View):
+def route_str(route):
+    if type(route) == str:
+        return route
+    else:return str(route.route)
+
+
+def path(url: str, clear: bool, view: View,middleware:Callable=None):
     """
     ```
     path(
@@ -15,7 +22,7 @@ def path(url: str, clear: bool, view: View):
     )
     ```
     """
-    return [url, clear, view]
+    return [url, clear, view, middleware] 
 
 
 class Routing:
@@ -49,37 +56,62 @@ class Routing:
     ```
     """
 
-    def __init__(self, page: Page, app_routes: list, not_found_view: View = ViewNotFound):
+    def __init__(self, page: Page, app_routes: list,middleware:Callable=None,not_found_view: View = ViewNotFound):
         self.page = page
         self.not_found_view = not_found_view
+        self.app_routes = app_routes
+        self.__middleware = middleware
         self.page.on_route_change = self.change_route
         self.page.on_view_pop = self.view_pop
-        self.app_routes = app_routes
-        self.params = Params({})
-        self._basket = Basket()
+        self.__params = Params({})
+        self.__basket = Basket()
 
     def change_route(self, route):
         notfound = True
         for url in self.app_routes:
             path_match = match(url[0], self.page.route)
             if path_match:
-                if url[1]:
+                self.__params = Params(path_match.groupdict())
+                if self.__middleware != None:
+                    self.__middleware( # call main middleware
+                        page=self.page,
+                        params=self.__params,
+                        basket=self.__basket
+                    )
+                if self.page.route != route_str(route=route):# if chnge route using main midellware recall change route
+                    self.page.go(self.page.route)
+                    return
+                
+                if url[3] != None:
+                    url[3]( # call url middleware
+                        page=self.page,
+                        params=self.__params,
+                        basket=self.__basket
+                    )
+                
+                if self.page.route != route_str(route=route):# if chnge route using url midellware recall change route
+                    self.page.go(self.page.route)
+                    return
+                
+
+                if url[1]: 
                     self.page.views.clear()
                 self.page.views.append(
                     url[2](
                         page=self.page,
-                        params=Params(path_match.groupdict()),
-                        basket=self._basket
+                        params=self.__params,
+                        basket=self.__basket
                     )
                 )
                 notfound = False
                 break
         if notfound:
+            self.__params = Params({"url": self.page.route})
             self.page.views.append(
                 self.not_found_view(
                     page=self.page,
-                    params=Params({"url": self.page.route}),
-                    basket=self._basket
+                    params=self.__params,
+                    basket=self.__basket
                 )
             )
         self.page.update()
@@ -88,3 +120,6 @@ class Routing:
         self.page.views.pop()
         top_view = self.page.views[-1]
         self.page.go(top_view.route)
+
+
+
