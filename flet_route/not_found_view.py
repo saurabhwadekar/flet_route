ab21f77f15ad2bb42d51b9ae3e782396b0afe1ca
Saurabh Wadekar <mr.saurabhwadekar@gmail.com>
2023-06-14 11:29:48 +0530

    async support available now

--- flet_route/not_found_view.py
+++ flet_route/not_found_view.py
@@ -1,5 +1,5 @@
 from flet import View, Page,Text,TextButton
-
+# import asyncio
 
 
 def ViewNotFound(page:Page,params,basket):
@@ -7,12 +7,31 @@ def ViewNotFound(page:Page,params,basket):
         "/notfound/404",
         controls=[
             Text(
-                "page not found error 404",
+                "View / Page not found error 404",
                 size=30,
             ),
             TextButton(
                 "Go Back OR '/' ",
                 on_click=lambda e:page.go(page.views[-2].route)
+            )
+        ]
+    )
+
+async def ViewNotFound_async(page:Page,params,basket):
+
+    async def go_back(e):
+        await page.go_async(page.views[-2].route)
+
+    return View(
+        "/notfound/404",
+        controls=[
+            Text(
+                "View / Page not found error 404",
+                size=30,
+            ),
+            TextButton(
+                "Go Back OR '/' ",
+                on_click=go_back
             )
         ]
     )
--- flet_route/routing.py
+++ flet_route/routing.py
@@ -1,6 +1,6 @@
 from flet import View, Page
 from repath import match
-from .not_found_view import ViewNotFound
+from .not_found_view import ViewNotFound,ViewNotFound_async
 from .params import Params
 from .basket import Basket
 from typing import Callable
@@ -12,7 +12,7 @@ def route_str(route):
     else:return str(route.route)
 
 
-def path(url: str, clear: bool, view: View,middleware:Callable=None):
+def path(url: str, clear: bool, view: Callable[[Page,Params,Basket],View],middleware:Callable[[Page,Params,Basket],None]=None):
     """
     ```
     path(
@@ -32,7 +32,7 @@ class Routing:
     import flet as ft
     from flet_route import Routing,path
 
-    def index_view(page,params):
+    def index_view(page,params,basket):
         return ft.View(
             "/",
             controls=[
@@ -49,22 +49,40 @@ class Routing:
         Routing(
             page=page, # Here you have to pass the page. Which will be found as a parameter in all your views
             app_routes=app_routes, # Here a list has to be passed in which we have defined app routing like app_routes
-            not_found_view:View=ViewNotFound # If you want that there should be a different view call when no path matches, then you can pass that view here. otherwise no need to give it it will call ViewNotFound by default
+            not_found_view = ViewNotFound # If you want that there should be a different view call when no path matches, then you can pass that view here. otherwise no need to give it it will call ViewNotFound by default
             )
         page.go(page.route)
     ft.app(target=main)
     ```
     """
 
-    def __init__(self, page: Page, app_routes: list,middleware:Callable=None,not_found_view: View = ViewNotFound):
+    def __init__(
+            self, 
+            page: Page, 
+            app_routes: list,
+            middleware:Callable[[Page,Params,Basket],None]=None,
+            async_is=False,
+            not_found_view: Callable[[Page,Params,Basket],View] = None
+            ):
+        
+        self.async_is = async_is
         self.page = page
-        self.not_found_view = not_found_view
         self.app_routes = app_routes
         self.__middleware = middleware
-        self.page.on_route_change = self.change_route
-        self.page.on_view_pop = self.view_pop
         self.__params = Params({})
         self.__basket = Basket()
+        if self.async_is:
+            self.page.on_route_change =  self.change_route_async
+            self.page.on_view_pop = self.view_pop_async
+            if not_found_view == None:
+                self.not_found_view = ViewNotFound_async
+            else:self.not_found_view = not_found_view
+        else:
+            self.page.on_route_change = self.change_route
+            self.page.on_view_pop = self.view_pop
+            if not_found_view == None:
+                self.not_found_view = ViewNotFound
+            else:self.not_found_view = not_found_view
 
     def change_route(self, route):
         notfound = True
@@ -120,6 +138,62 @@ class Routing:
         self.page.views.pop()
         top_view = self.page.views[-1]
         self.page.go(top_view.route)
+
+    async def change_route_async(self, route):
+        notfound = True
+        for url in self.app_routes:
+            path_match = match(url[0], self.page.route)
+            if path_match:
+                self.__params = Params(path_match.groupdict())
+                if self.__middleware != None:
+                    await self.__middleware( # call main middleware
+                        page=self.page,
+                        params=self.__params,
+                        basket=self.__basket
+                    )
+                if self.page.route != route_str(route=route):# if chnge route using main midellware recall change route
+                    await self.page.go_async(self.page.route)
+                    return
+                
+                if url[3] != None:
+                    await url[3]( # call url middleware
+                        page=self.page,
+                        params=self.__params,
+                        basket=self.__basket
+                    )
+                
+                if self.page.route != route_str(route=route):# if chnge route using url midellware recall change route
+                    await self.page.go_async(self.page.route)
+                    return
+                
+
+                if url[1]: 
+                    self.page.views.clear()
+                self.page.views.append(
+                    await url[2](
+                        page=self.page,
+                        params=self.__params,
+                        basket=self.__basket
+                    )
+                )
+                notfound = False
+                break
+        if notfound:
+            self.__params = Params({"url": self.page.route})
+            self.page.views.append(
+                await self.not_found_view(
+                    page=self.page,
+                    params=self.__params,
+                    basket=self.__basket
+                )
+            )
+        await self.page.update_async()
+
+    async def view_pop_async(self, view):
+        self.page.views.pop()
+        top_view = self.page.views[-1]
+        await self.page.go_async(top_view.route)
+
 
 
 
